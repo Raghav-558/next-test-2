c30a069e4d49dc9b95a48d914a4f99326365b12c
Raghav-558 <jangraraghav9211@gmail.com>
2025-02-17 16:50:00 +0530

    make slider

--- src/components/QuestionOne.tsx
+++ src/components/QuestionOne.tsx
@@ -10,7 +10,7 @@ gsap.registerPlugin(ScrollTrigger);
 const AiSlider = () => {
   useEffect(() => {
     gsap.to(".slider-container", {
-      xPercent: -100 * (SLIDER_LIST.length - 1),
+      xPercent: -105 * (SLIDER_LIST.length - 1),
       ease: "none",
       scrollTrigger: {
         trigger: ".slider-container",
@@ -30,7 +30,7 @@ const AiSlider = () => {
           alt="shadow"
           width={199}
           height={199}
-          className="absolute top-10 right-0"
+          className="absolute top-10 right-0 pointer-events-none"
         />
         <h2 className="max-w-[830px] mx-auto font-medium text-5xl leading-[57.6px] text-white text-center max-md:text-3xl max-md:leading-[36px]">
           Transforming Secure, Modern{" "}
@@ -50,7 +50,7 @@ const AiSlider = () => {
           ))}
         </div>
       </div>
-      <div className="slider-container flex flex-nowrap w-full lg:pt-[60px] !px-4">
+      <div className="slider-container flex flex-nowrap w-full pt-[60px] max-lg:pt-10 gap-8 !px-4 max-md:gap-4">
         {SLIDER_LIST.map((obj, i) => (
           <div
             key={i}
@@ -62,19 +62,19 @@ const AiSlider = () => {
                 alt="number"
                 width={297}
                 height={182}
-                className="mb-6 max-lg:max-w-[100px] max-md:max-w-[50px] pointer-events-none"
+                className="mb-6 max-lg:max-w-[250px] max-md:max-w-[150px] pointer-events-none"
               />
-              <p className="text-[32px] font-bold leading-[39px] pb-4 text-white max-md:text-[24px] max-md:leading-[30px]">
+              <p className="text-[32px] font-bold leading-[39px] pb-4 text-white max-lg:text-[24px] max-lg:leading-[30px]">
                 {obj.title}{" "}
-                <span className="bg-gradient-to-l to-lightSky from-lightPurple bg-clip-text text-transparent text-[32px] font-bold leading-[39px] max-md:text-[24px] max-md:leading-[30px]">
+                <span className="bg-gradient-to-l to-lightSky from-lightPurple bg-clip-text text-transparent text-[32px] font-bold leading-[39px] max-lg:text-[24px] max-lg:leading-[30px]">
                   {obj.gradientText}
                 </span>
               </p>
-              <p className="font-poppins text-white leading-[25px] opacity-90 max-md:text-[18px] max-md:leading-[22px]">
+              <p className="font-poppins text-white leading-[25px] opacity-90 max-lg:text-[18px] max-lg:leading-[22px]">
                 {obj.description}
               </p>
               {obj.secondDescription && (
-                <p className="font-poppins text-white leading-[25px] opacity-90 max-md:text-[18px] max-md:leading-[22px]">
+                <p className="font-poppins text-white leading-[25px] opacity-90 max-lg:text-[18px] max-lg:leading-[22px]">
                   {obj.secondDescription}
                 </p>
               )}
@@ -85,7 +85,7 @@ const AiSlider = () => {
                 alt={obj.imageAlt}
                 height={427}
                 width={614}
-                className="shadow-[0px_4px_58.7px_0px_#00DDFF26] rounded-xl h-[417px] max-lg:max-w-[614px] max-lg:max-h-[417px] max-md:flex max-md:justify-center"
+                className="shadow-[0px_4px_58.7px_0px_#00DDFF26] pointer-events-none rounded-xl h-[417px] max-lg:max-w-[480px] max-lg:max-h-[417px] max-md:flex max-md:justify-center"
               />
             </div>
           </div>
